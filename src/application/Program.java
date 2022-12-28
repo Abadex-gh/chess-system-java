4ff93e0d34438a99380b9f9c02e20576bca070cc
Abadex-gh <alexsanderabade@hotmail.com>
2022-12-28 18:47:18 -0300

    Changes done

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,7 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("Hello world - again -");
-		System.out.println("Hello world - ainda for the third time");
+		System.out.println("Hello world again");
 	}
 
 }
